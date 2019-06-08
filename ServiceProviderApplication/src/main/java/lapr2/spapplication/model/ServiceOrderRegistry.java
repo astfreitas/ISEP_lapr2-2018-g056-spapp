80b973084af37770784bcd966e635e480c69f187
Josué Mota <1171668@isep.ipp.pt>
2019-06-08 15:58:36 +0000

    [UC-16] [implementation] main methods completed | still requires the logic for import adapters

--- ServiceProviderApplication/src/main/java/lapr2/spapplication/model/ServiceOrderRegistry.java
+++ ServiceProviderApplication/src/main/java/lapr2/spapplication/model/ServiceOrderRegistry.java
@@ -3,19 +3,45 @@ package lapr2.spapplication.model;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class ServiceOrderRegistry {
-    
+
     private List<ServiceOrder> serviceOrders;
 
     public ServiceOrderRegistry() {
         this.serviceOrders = new ArrayList();
     }
 
+    /**
+     * 
+     * @param filePath
+     * @param serviceOrderImporter 
+     */
     public void importData(String filePath, ServiceOrderImporter serviceOrderImporter) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<ServiceOrder> importedServiceOrders = serviceOrderImporter.importOrders(filePath);
+        for (ServiceOrder serOrder : importedServiceOrders) {
+            if (validate(serOrder)) {
+                addServiceOrder(serOrder);
+            }
+        }
     }
-    
-    
-            
+
+    /**
+     * Method will vefiry if the service order already exists in the Service
+     * order list.
+     * @param ServiceOrder
+     * @return true if the service order was not added yet.
+     */
+    private boolean validate(ServiceOrder servOrder) {
+        return !serviceOrders.contains(servOrder);
+    }
+
+    /**
+     * Method that will add a service order to the container
+     * @param serOrder 
+     * @return true if the operation was a success.
+     */
+    private boolean addServiceOrder(ServiceOrder serOrder) {
+        return serviceOrders.add(serOrder);
+    }
+
 }
